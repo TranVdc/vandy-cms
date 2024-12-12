6bf01f41c1e3221e185a98670cf6b00e4a3a1348
DESKTOP-S3C5VP0\TRANVDC <tranvanduoc2394@gmail.com>
2024-12-12 21:06:14 +0700

    applied clerk

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DashboardWrapper from "./dashboardWrapper";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignIn
+} from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +24,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <DashboardWrapper>{children}</DashboardWrapper>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <main className="main-container">
+          <SignedOut>
+            <div className="center-container">
+                <SignIn routing="hash" />
+              </div>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+            <DashboardWrapper>{children}</DashboardWrapper>
+          </SignedIn>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
